7375d5434761df7057dd207f428757fd2d140c8c
Tarmo Korõtko <41905628+tarmokorotko@users.noreply.github.com>
2025-12-15 16:21:04 +0200

    Change logging to debug and improve prognosis checks
    Refactor logging levels and enhance prognosis handling.

--- query_utils.py
+++ query_utils.py
@@ -1,4 +1,5 @@
 from Query import Query
+import Util
 
 _query_url = None
 _query_headers = None
@@ -9,7 +10,7 @@ def init(url, headers, logger=None):
     _query_headers = headers
     _logger = logger
     
-    logger.info(f"query_utils initialized with URL: {_query_url}")
+    logger.debug(f"query_utils initialized with URL: {_query_url}")
     
 # Helper to create Query object
 def Q():
@@ -27,7 +28,18 @@ def get_datapoint(dp_identifier):
         .paginate(page=0, size=1)
         .get("/datapoints")
     )
+    _logger.debug("get_datapoint(%s) -> %s", dp_identifier, dp_data)
     return dp_data
+
+# GET datapoint ID
+def get_datapoint_ID(dp_identifier):
+    dp_data = (
+        Q()
+        .filter(identifier__equals=dp_identifier)
+        .paginate(page=0, size=1)
+        .get("/datapoints")
+    )
+    return dp_data[0]["id"]
 
 # Get datapoint last reading by identifier
 def get_last_reading(dp_identifier):
@@ -47,8 +59,38 @@ def get_last_reading_value(dp_identifier):
     last_reading_value = get_last_reading(dp_identifier)
     return last_reading_value[0].get("value")
 
+# Get datapoint last control command by identifier
+def get_last_control(dp_identifier):
+    dp_data = get_datapoint(dp_identifier)
+    dp_id = dp_data[0]["id"]
+    last_control_val = (
+        Q()
+        .filter(datapointId__equals=dp_id)
+        .order_by("time", "desc")
+        .paginate(page=0, size=1)
+        .get("/control-values")
+    )
+    return last_control_val
+
+# Get datapoint last control command value by identifier
+def get_last_control_value(dp_identifier):
+    last_control_value = get_last_control(dp_identifier)
+    return last_control_value[0].get("value")
+
+# Get datapoint last control command status by identifier
+def get_last_control_status(dp_identifier):
+    last_reading_value = get_last_control(dp_identifier)
+    return last_reading_value[0].get("sent")
+
+# Get datapoint last control command value and status by identifier
+def get_last_control_value_and_status(dp_identifier):
+    last_reading_value = get_last_control(dp_identifier)
+    return {
+        "value" : last_reading_value[0].get("value"), 
+        "sent" : last_reading_value[0].get("sent")}
+
 # GET datapoint last prognosis readings data
-def get_last_prognosis_readings(dp_identifier):
+def get_last_prognosis_readings(dp_identifier, generate_if_missing=False):
     last_prognosis_id = get_datapoint(dp_identifier)[0].get("lastPrognosisId")
     if last_prognosis_id is not None:
         last_prognosis_readings = (
@@ -56,25 +98,31 @@ def get_last_prognosis_readings(dp_identifier):
             .filter(datapointPrognosisId__equals=last_prognosis_id)
             .get("/prognosis-readings")
         )
-        return last_prognosis_readings
-
-    else:
-        _logger.warning("No prognosis available for this datapoint.")
-        return []
+        if not last_prognosis_readings:
+            raise RuntimeError(f"No prognosis readings found for lastPrognosisId={last_prognosis_id}")
+    else:              
+        _logger.warning(f"No prognosis available for datapoint {dp_identifier}.")
+        if generate_if_missing:
+            last_prognosis_readings = Util.generate_prognosis_entries() 
+        else:
+            last_prognosis_readings = []
+    
+    return last_prognosis_readings    
         
 # GET datapoint's last datapoint prognosis
 def get_datapoint_prognosis(dp_identifier):
     last_prognosis_id = get_datapoint(dp_identifier)[0].get("lastPrognosisId")
+    _logger.debug("lastPrognosisId = %s", last_prognosis_id)
     if last_prognosis_id is not None:
         datapoint_prognosis = (
             Q()
             .filter(Id__equals=last_prognosis_id)
             .get("/datapoint-prognoses")
         )
-        _logger.info(f"datapoint_prognosis: {datapoint_prognosis}")
+        _logger.debug(f"datapoint_prognosis: {datapoint_prognosis}")
         return datapoint_prognosis
     else:
-        _logger.warning("No prognosis available for this datapoint.")
+        _logger.warning(f"No prognosis available for datapoint {dp_identifier}.")
         return None
         
 ##########################################################        
@@ -94,5 +142,23 @@ def post_datapoint_prognosis(prognosis_payload):
     for dp_pr_id in prognosis_readings_payload:
         dp_pr_id["datapointPrognosisId"] = response["id"]
     post_prognosis_readings(prognosis_readings_payload)
+    
+    return response
+
+# POST datapoint reading
+def post_datapoint_reading(datapoint_reading_payload):    
+    response = (Q().post("/readings", json=datapoint_reading_payload))
+    
+    return response
+
+# POST datapoint control value
+def post_datapoint_ctrl_value(datapoint_ctrl_val_payload):    
+    response = (Q().post("/control-values", json=datapoint_ctrl_val_payload))
+    
+    return response
+
+# POST set datapoint control value status to sent
+def post_datapoint_ctrl_status_sent(ctrl_status_sent_payload):    
+    response = (Q().post("/control-values/set-sent", json=ctrl_status_sent_payload))
     
     return response
