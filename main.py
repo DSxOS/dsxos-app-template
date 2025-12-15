3587b44f0cda55fc19e6a4089d72a599a611eb61
Tarmo Korõtko <41905628+tarmokorotko@users.noreply.github.com>
2025-12-15 16:20:26 +0200

    Refactor main.py for improved logging and argument handling
    Updated logging and argument parsing for better clarity and debugging.

--- main.py
+++ main.py
@@ -2,41 +2,51 @@ import query_utils
 import argparse
 import yaml
 from datetime import datetime, timezone
-import Util
+import pytz
 from logger import setup_logger
 
+#######################################################################
+#### INITIALIZATION
+#######################################################################
+APP_NAME = "dsxos-app-test"
 
-# create parser
-parser = argparse.ArgumentParser(description="Run dsxos-app-test with config file")
+# Create parser
+parser = argparse.ArgumentParser(description=f"Run {APP_NAME} with config file")
 parser.add_argument("-c", "--config", required=False, help="Path to config YAML file", default="/app/config.yaml")
-args = parser.parse_args() # Read arguments
-with open(args.config, "r") as f: # Open and read config-file
+args = parser.parse_args()
+with open(args.config, "r") as f: 
     raw_data = yaml.safe_load(f)
     
 # Extract API URL and Token
-api_url = raw_data["params"]["apiEndpoint"]
-api_token = raw_data["params"]["token"]
+api_url = raw_data['params']['apiEndpoint']
+api_token = raw_data['params']['token']
 api_headers = {"Authorization": api_token}
 
 # Initialize logger with central logging to Loki
 logger = setup_logger(
-    #app_name="dsxos-app-test",
     log_file="query.log",
     loki_url="http://localhost:3100/loki/api/v1/push",  # Loki address
-    loki_tags={"app_name": "dsxos-app-test"},        # add more tags if needed
-    level="INFO"
+    loki_tags={"app_name": APP_NAME},        # add more tags if needed
+    level=raw_data["logLevel"]    
 )
 
 # Initialize query_utils with URL + headers    
 query_utils.init(api_url, api_headers, logger)
 
-# Log passed arguments 
-logger.info("Passed arguments: %s", raw_data)
+# Log passed arguments for debugding
+logger.debug(f"{APP_NAME} run with arguments: %s", raw_data)
 
-# Hello wolrld application
-logger.info("dsxos-app-test start")
+#######################################################################
+#### APPLICATION
+#######################################################################
+try:
+    # Your application logger with exception handling
+    
+except Exception as e:
+    # Exception handling
+    # logger.error(f'Error generating ESS schedule: {e}')
 
-start_time = datetime.now(timezone.utc)
-logger.info(f'Hello world. The time is {start_time.strftime("%H:%M:%S %d-%m-%Y")}')
-
-logger.info("dsxos-app-test finished")
+#######################################################################
+#### FINALIZATION
+#######################################################################
+logger.info(f"{APP_NAME} executed successfully")
